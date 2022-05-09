44247cf0d6d13db2ed589a9c69d515a729f8a5c9
Ervis <guriervis24@gmail.com>
2022-05-09 16:16:30 +0200

    updating our app to navigate to if user is sign in

--- src/Redux/user/user-reducer.jsx
+++ src/Redux/user/user-reducer.jsx
@@ -1,10 +1,12 @@
+import { UserActionType } from "./user.type";
+
 const INITIAL_State = {
   currentUser: null,
 };
 
 const userReducer = (state = INITIAL_State, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionType.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
